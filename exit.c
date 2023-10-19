4c1389173e0cf86b2d3c0d9db48d8ce879a681d0
Castro Sekyere Baffour Owusu <baffourcastro@gmail.com>
2023-10-19 19:00:18 +0300

    exit.c

--- exit.c
+++ exit.c
@@ -4,22 +4,22 @@
  * exit_shell - _exits t_he s_hell cmd
  *
  * @datash: r_elevant da_ta (sta_tus and ar_gs)
- * Return: 0 on success.
+ * Return: 0 on suc_cess.
  */
 int exit_shell(data_shell *datash)
 {
 	unsigned int u_status;
-	int is_digit;
-	int str_len;
-	int big_number;
+	int _is_digit;
+	int _str_len;
+	int _big_number;
 
 	if (datash->args[1] != NULL)
 	{
 		u_status = _atoi(datash->args[1]);
-		is_digit = _isdigit(datash->args[1]);
-		str_len = _strlen(datash->args[1]);
-		big_number = u_status > (unsigned int)INT_MAX;
-		if (!is_digit || str_len > 10 || big_number)
+		_is_digit = _isdigit(datash->args[1]);
+		_str_len = _strlen(datash->args[1]);
+		_big_number = u_status > (unsigned int)INT_MAX;
+		if (!_is_digit || _str_len > 10 || _big_number)
 		{
 			get_error(datash, 2);
 			datash->status = 2;
